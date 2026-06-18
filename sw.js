92030badf1a88867fdc419c5a61a42662a32d59d
Muyiwa Oladimeji <muyiwaoladimeji0@gmail.com>
2026-06-18 21:31:26 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = '3080-v1';
+const assets = [
+  '/',
+  '/index.html'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
